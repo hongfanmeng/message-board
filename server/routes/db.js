2bfa114d87e2d99e081344b46f663a756ff9e42e
hongfanmeng <hongfanmeng@gmail.com>
2020-05-04 18:44:48 +0800

    it should be ok

--- server/routes/db.js
+++ server/routes/db.js
@@ -1,8 +1,11 @@
 const { MongoClient, ObjectID } = require("mongodb");
 let db;
+const uri =
+  "mongodb://user:user@cluster0-shard-00-00-oan8j.azure.mongodb.net:27017,cluster0-shard-00-01-oan8j.azure.mongodb.net:27017,cluster0-shard-00-02-oan8j.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 module.exports = async () => {
-  client = await MongoClient.connect("mongodb://localhost:27017/myposts");
-  db = client;
+  await MongoClient.connect(uri, function (err, client) {
+    db = client.db("myposts");
+  });
 };
 
 module.exports.Posts = {
